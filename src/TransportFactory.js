defa0daa81ef0ef4b3936c319dd284f408de230e
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2015-05-26 18:54:51 +0200

    Adds TransportFactory

---
+++ src/TransportFactory.js
@@ -0,0 +1,49 @@
+import AjaxTransport from './AjaxTransport';
+
+/**
+ * Provides a factory for data transport.
+ */
+class TransportFactory {
+
+  constructor() {
+    this.transports = {};
+    this.transports[TransportFactory.DEFAULT_TRANSPORT_NAME] = AjaxTransport;
+  }
+
+  /**
+   * Returns {@code TransportFactory} instance.
+   */
+  static instance() {
+    if (!TransportFactory.instance_) {
+      TransportFactory.instance_ = new TransportFactory();
+    }
+    return TransportFactory.instance_;
+  }
+
+  get(implementationName) {
+    var transportClass = this.transports[implementationName];
+
+    if (transportClass == null) {
+      throw new Error('Invalid transport name: ' + implementationName);
+    }
+
+    try {
+      return new (transportClass)();
+    }
+    catch (err) {
+      throw new Error('Can\'t create transport', err);
+    }
+  }
+
+  /**
+   * Returns default transport.
+   */
+  getDefault() {
+    return this.get(TransportFactory.DEFAULT_TRANSPORT_NAME);
+  }
+
+}
+
+TransportFactory.DEFAULT_TRANSPORT_NAME = 'default';
+
+export default TransportFactory;
---
+++ test/TransportFactory.js
@@ -0,0 +1,24 @@
+import TransportFactory from '../src/TransportFactory';
+
+describe('TransportFactory', function () {
+
+  it('should instance() be singleton', function() {
+    var instance = TransportFactory.instance();
+    assert.strictEqual(instance, TransportFactory.instance());
+  });
+
+  it('should throws exception for error on creating transport', function() {
+    TransportFactory.instance().transports['willThrowError'] = function() {
+      throw new Error();
+    };
+    assert.throws(function() {
+      TransportFactory.instance().get('willThrowError');
+    }, Error);
+  });
+
+  it('should throws exception for invalid transport type', function() {
+    assert.throws(function() {
+      TransportFactory.instance().get('invalid');
+    }, Error);
+  });
+});
