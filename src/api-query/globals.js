897abf1099f4fce232533ba9c25ad117bcab5492
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2015-08-31 19:23:40 -0700

    Import api query classes

--- src/api-query/globals.js
+++ src/api-query/globals.js
@@ -1,7 +1,15 @@
-import Query from './Query';
 import Filter from './Filter';
+import Geo from './Geo';
+import Query from './Query';
+import Range from './Range';
+import Search from './Search';
+import SearchFilter from './SearchFilter';
 
 if (typeof window !== undefined) {
-	window.Query = Query;
 	window.Filter = Filter;
+	window.Geo = Geo;
+	window.Query = Query;
+	window.Range = Range;
+	window.Search = Search;
+	window.SearchFilter = SearchFilter;
 }
