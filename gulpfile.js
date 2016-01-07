3059ee2b11b439b83f90a28329825ba47eecea38
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2016-01-07 11:48:04 -0300

    SF

--- gulpfile.js
+++ gulpfile.js
@@ -115,10 +115,9 @@ gulp.task('ci', function(cb) {
 	if (process.env.SAUCE_USERNAME) {
 		return runSequence('lint', 'test:node', 'test:saucelabs', 'build:node', 'build', cb);
 	}
-
-	console.warn('Not running tests on browsers (most likely due to security restrictions)');
+	console.warn('Not running tests (most likely due to security restrictions)');
 	console.warn('See https://docs.travis-ci.com/user/sauce-connect/ for help');
-	return runSequence('lint', 'test:node', 'build:node', 'build', cb);
+	cb();
 });
 
 gulp.task('build:node', function() {
--- test/fixtures/browser/AjaxRequestMock.js
+++ test/fixtures/browser/AjaxRequestMock.js
@@ -1,43 +1,37 @@
 'use strict';
 
+import async from 'bower:metal/src/async/async';
+
 class AjaxRequestMock {
 	static intercept() {
 		return this;
 	}
 
-	static timeout() {
-		AjaxRequestMock.timeout_ = true;
-	}
-
 	static reply(status, body, headers) {
 		AjaxRequestMock.status = status;
 		AjaxRequestMock.headers = headers;
-		AjaxRequestMock.body = body || '';
+		AjaxRequestMock.body = body;
 	}
 
 	static get() {
-		var xhr = AjaxRequestMock.fakeServer.requests;
-		return xhr && xhr[0];
+		if (AjaxRequestMock.fakeServer.requests) {
+			return AjaxRequestMock.fakeServer.requests[0];
+		}
 	}
 
 	static setup() {
 		AjaxRequestMock.fakeServer = sinon.fakeServer.create();
-
-		// must wait reaching each test to get what to respond to the mocked request
-		setTimeout(() => {
-			if (AjaxRequestMock.status && !AjaxRequestMock.timeout_) {
-				AjaxRequestMock.fakeServer.respondWith([
-					AjaxRequestMock.status,
-					AjaxRequestMock.headers,
-					AjaxRequestMock.body
-				]);
-				AjaxRequestMock.fakeServer.respond();
-			}
-		}, 0);
+		async.nextTick(() => {
+			AjaxRequestMock.fakeServer.respondWith([
+				AjaxRequestMock.status,
+				AjaxRequestMock.headers,
+				AjaxRequestMock.body || ''
+			]);
+			AjaxRequestMock.fakeServer.respond();
+		});
 	}
 
 	static teardown() {
-		AjaxRequestMock.timeout_ = undefined;
 		AjaxRequestMock.fakeServer.restore();
 	}
 }
