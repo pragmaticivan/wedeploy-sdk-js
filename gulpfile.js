643628d27f87288b12d190415ef6f8ab20048fdf
Henrique Vicente <henriquevicente@gmail.com>
2016-01-07 00:34:40 -0300

    Adding SauceLabs capabilities matrix based on supported browsers.

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,63 @@ var merge = require('merge-stream');
 metal.registerTasks({
 	globalName: 'launchpad',
 	buildSrc: ['src/**/!(node)/*.js'],
-	bundleFileName: 'api.js'
+	bundleFileName: 'api.js',
+	testSaucelabsBrowsers: {
+		sl_chrome: {
+			base: 'SauceLabs',
+			browserName: 'chrome'
+		},
+		sl_safari_8: {
+			base: 'SauceLabs',
+			browserName: 'safari',
+			version: '8'
+		},
+		sl_safari_9: {
+			base: 'SauceLabs',
+			browserName: 'safari',
+			version: '9'
+		},
+		sl_firefox: {
+			base: 'SauceLabs',
+			browserName: 'firefox'
+		},
+		sl_ie_10: {
+			base: 'SauceLabs',
+			browserName: 'internet explorer',
+			platform: 'Windows 7',
+			version: '10'
+		},
+		sl_ie_11: {
+			base: 'SauceLabs',
+			browserName: 'internet explorer',
+			platform: 'Windows 8.1',
+			version: '11'
+		},
+		sl_edge_20: {
+			base: 'SauceLabs',
+			browserName: 'microsoftedge',
+			platform: 'Windows 10',
+			version: '20'
+		},
+		sl_iphone: {
+			base: 'SauceLabs',
+			browserName: 'iphone',
+			platform: 'OS X 10.10',
+			version: '9.2'
+		},
+		sl_android_4: {
+			base: 'SauceLabs',
+			browserName: 'android',
+			platform: 'Linux',
+			version: '4.4'
+		},
+		sl_android_5: {
+			base: 'SauceLabs',
+			browserName: 'android',
+			platform: 'Linux',
+			version: '5.0'
+		}
+	}
 });
 
 gulp.task('test:node', function() {
