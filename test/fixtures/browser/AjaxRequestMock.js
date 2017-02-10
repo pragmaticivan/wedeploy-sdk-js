c5c50154a59a5bfc47d07d840b128700cfdb04a9
Iliyan Peychev <iliyan.peychev@liferay.com>
2017-02-10 18:23:42 +0100

    Update gulp-metal to v1.11.2 (#96)
    * Revert "Discard events converting in tests (#90)"
    
    This reverts commit 1949ef1d3dd8935426cf0b75e1085999c6f16e87.
    
    * Update gulp-metal to v1.11.2
    
    Fixes #95

--- test/fixtures/browser/AjaxRequestMock.js
+++ test/fixtures/browser/AjaxRequestMock.js
@@ -16,6 +16,7 @@ class AjaxRequestMock {
 	static get() {
 		if (AjaxRequestMock.fakeServer.requests) {
 			var request = AjaxRequestMock.fakeServer.requests[0];
+			convertEvents_(request);
 			return request;
 		}
 	}
@@ -28,6 +29,7 @@ class AjaxRequestMock {
 		AjaxRequestMock.fakeServer = sinon.fakeServer.create();
 		AjaxRequestMock.addedEvents = false;
 		async.nextTick(() => {
+			convertEvents_(AjaxRequestMock.get());
 			AjaxRequestMock.fakeServer.respondWith([
 				AjaxRequestMock.status,
 				AjaxRequestMock.headers,
@@ -39,6 +41,27 @@ class AjaxRequestMock {
 
 	static teardown() {
 		AjaxRequestMock.fakeServer.restore();
+	}
+}
+
+/**
+ * The most recent version of sinon is dealing with XMLHttpRequest events in a
+ * very weird way. They fire `error` or status codes that the browser fires
+ * `load` for, and fire `abort` for cases where the browser fires `error`.
+ * This is a simple hack converting the events to the ones expected as coming
+ * from the browser.
+ */
+function convertEvents_(request) {
+	if (!AjaxRequestMock.addedEvents && request) {
+		AjaxRequestMock.addedEvents = true;
+		request.addEventListener('error', function(event) {
+			event.stopPropagation();
+			request.dispatchEvent(new sinon.ProgressEvent('load', event, request));
+		});
+		request.addEventListener('abort', function(event) {
+			event.stopPropagation();
+			request.dispatchEvent(new sinon.ProgressEvent('error', event, request));
+		});
 	}
 }
 
